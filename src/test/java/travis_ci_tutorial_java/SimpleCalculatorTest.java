f80585bb1287352d292226ebbe781144da49984b
manaswiniutla <sutla1@students.towson.edu>
2026-03-03 23:50:05 -0500

    Update SimpleCalculatorTest.java

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,21 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+	@Test
+    public void testMinus() {
+    	SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(calc.minus(3, 2), 1);
+    }
+
+    @Test
+    public void testMultiply() {
+    	SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(calc.multiply(2, 3), 6);
+    }
+
+    @Test
+    public void testDivide() {
+    	SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(calc.divide(6, 3), 2);
+    }
 }
