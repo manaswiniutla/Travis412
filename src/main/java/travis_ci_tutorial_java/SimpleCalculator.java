ba8d7dd40815994285ed7943b159e8fb9779fe73
admin <manaswiniutla@gmail.com>
2026-03-03 19:45:08 -0500

    changes in test add in test file

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -3,5 +3,19 @@ package travis_ci_tutorial_java;
 public class SimpleCalculator {
 	public int add(int a, int b) {
 		return a + b;
-	}
+	}	
+//    public int minus(int a, int b) {
+//        return a - b;
+//    }
+//
+//    public int multiply(int a, int b) {
+//        return a * b;
+//    }
+//
+//    public int divide(int a, int b) {
+//        if (b == 0) {
+//            throw new IllegalArgumentException("Cannot divide by zero");
+//        }
+//        return a / b;
+//    }
 }
--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,22 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 0);
 	}
+	
+//    @Test
+//    public void testMinus() {
+//    	SimpleCalculator calc = new SimpleCalculator();
+//        assertEquals(calc.minus(3, 2), 1);
+//    }
+//
+//    @Test
+//    public void testMultiply() {
+//    	SimpleCalculator calc = new SimpleCalculator();
+//        assertEquals(calc.multiply(2, 3), 6);
+//    }
+//
+//    @Test
+//    public void testDivide() {
+//    	SimpleCalculator calc = new SimpleCalculator();
+//        assertEquals(calc.divide(6, 3), 2);
+//    }
 }
